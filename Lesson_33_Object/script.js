8234bb41483fcd14bbff5dd1c42baecdb313586b
Martiniuk Rostislav <roxst1k@gmail.com>
2023-03-01 13:08:10 +0200

    Add lesson 33 Object

--- Lesson_33_Object/script.js
+++ Lesson_33_Object/script.js
@@ -43,57 +43,125 @@
 // car.isDriver(prompt('Check your driver name'));
 // car.getTimeTravel(+prompt('Enter your distance in kilometers'));
 
-let time = {
-    hour : '',
-    minutes: '',
-    second : '',
+// let time = {
+//     hour : '',
+//     minutes: '',
+//     second : '',
+//
+//     getTime : function () {
+//         let date = new Date;
+//         this.hour = date.getHours();
+//         this.minutes = date.getMinutes();
+//         this.second = date.getSeconds();
+//         console.log(`Now ${this.hour.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.second.toString().padStart(2, '0')}`);
+//     },
+//
+//     addSeconds : function (seconds) {
+//         let date = new Date;
+//         this.second = date.getSeconds() + seconds;
+//         time.correctTime();
+//         console.log(`Your new time is ${this.hour.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.second.toString().padStart(2, '0')}`);
+//     },
+//
+//     addMinutes : function (minutes) {
+//         let date = new Date;
+//         this.minutes = date.getMinutes() + minutes;
+//         time.correctTime()
+//         console.log(`Your new time is ${this.hour.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.second.toString().padStart(2, '0')}`);
+//     },
+//
+//     addHour : function (hour) {
+//         let date = new Date;
+//         this.hour = date.getHours() + hour;
+//         time.correctTime();
+//         console.log(`Your new time is ${this.hour.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.second.toString().padStart(2, '0')}`);
+//     },
+//
+//     correctTime : function () {
+//         if (this.second >= 60) {
+//             this.minutes = Math.floor(this.second / 60) + this.minutes;
+//             this.second = this.second % 60;
+//         }
+//         if (this.minutes >= 60) {
+//             this.hour = Math.floor(this.minutes / 60) + this.hour;
+//             this.minutes = this.minutes % 60;
+//         }
+//         if (this.hour >= 24) {
+//             this.hour = this.hour % 24;
+//         }
+//     }
+//
+// }
+//
+// time.getTime();
+// // time.addSeconds()
+// // time.addMinutes(45);
+// time.addHour(2);
 
-    getTime : function () {
-        let date = new Date;
-        this.hour = date.getHours();
-        this.minutes = date.getMinutes();
-        this.second = date.getSeconds();
-        console.log(`Now ${this.hour.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.second.toString().padStart(2, '0')}`);
+let firstFraction = {
+    numerator : null,
+    denominator : null,
+    addNumber : function () {
+        this.numerator = +prompt(`First numerator: `);
+        this.denominator = +prompt(`First denominator: `);
+        console.log(`You first fraction is ${this.numerator} / ${this.denominator} `);
     },
-
-    addSeconds : function (seconds) {
-        let date = new Date;
-        this.second = date.getSeconds() + seconds;
-        time.correctTime();
-        console.log(`Your new time is ${this.hour.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.second.toString().padStart(2, '0')}`);
-    },
-
-    addMinutes : function (minutes) {
-        let date = new Date;
-        this.minutes = date.getMinutes() + minutes;
-        time.correctTime()
-        console.log(`Your new time is ${this.hour.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.second.toString().padStart(2, '0')}`);
-    },
-
-    addHour : function (hour) {
-        let date = new Date;
-        this.hour = date.getHours() + hour;
-        time.correctTime();
-        console.log(`Your new time is ${this.hour.toString().padStart(2, '0')}:${this.minutes.toString().padStart(2, '0')}:${this.second.toString().padStart(2, '0')}`);
-    },
-
-    correctTime : function () {
-        if (this.second >= 60) {
-            this.minutes = Math.floor(this.second / 60) + this.minutes;
-            this.second = this.second % 60;
-        }
-        if (this.minutes >= 60) {
-            this.hour = Math.floor(this.minutes / 60) + this.hour;
-            this.minutes = this.minutes % 60;
-        }
-        if (this.hour >= 24) {
-            this.hour = this.hour % 24;
-        }
-    }
-
 }
 
-time.getTime();
-// time.addSeconds()
-time.addMinutes(45)
-// time.addHour();
+let secondFraction = {
+    numerator : null,
+    denominator : null,
+    addNumber : function () {
+        this.numerator = +prompt(`Second numerator: `);
+        this.denominator = +prompt(`Second denominator: `);
+        console.log(`You second fraction is ${this.numerator} / ${this.denominator} `);
+    },
+}
+
+let newFraction = {
+    numerator : null,
+    denominator : null,
+    sum : function () {
+        if (firstFraction.denominator === secondFraction.denominator) {
+            this.numerator = firstFraction.numerator + secondFraction.numerator;
+            this.denominator = firstFraction.denominator;
+            getRounding(this.numerator,this.denominator);
+        } else if (firstFraction.denominator > secondFraction.denominator) {
+            if (firstFraction.denominator % secondFraction.denominator === 0) {
+                this.denominator = firstFraction.denominator;
+                this.numerator = firstFraction.numerator + secondFraction.numerator * (firstFraction.denominator / secondFraction.denominator);
+            } else {
+                this.denominator = firstFraction.denominator * secondFraction.denominator;
+                this.numerator = firstFraction.numerator * secondFraction.denominator + secondFraction.numerator * firstFraction.denominator;
+                }
+            getRounding(this.numerator,this.denominator);
+        } else {
+            if (secondFraction.denominator % firstFraction.denominator === 0) {
+                this.denominator = secondFraction.denominator;
+                this.numerator = secondFraction.numerator + firstFraction.numerator * (secondFraction.denominator / firstFraction.denominator);
+            } else {
+                this.denominator = firstFraction.denominator * secondFraction.denominator;
+                this.numerator = firstFraction.numerator * secondFraction.denominator + secondFraction.numerator * firstFraction.denominator;
+            }
+            getRounding(this.numerator,this.denominator);
+        }
+
+    }
+}
+
+firstFraction.addNumber();
+secondFraction.addNumber();
+newFraction.sum();
+
+function getRounding (numerator, denominator) {
+    while (numerator % 3 === 0 && denominator % 3 === 0 ) {
+        numerator = numerator / 3;
+        denominator = denominator / 3;
+    }
+    while (numerator % 2 === 0 && denominator % 2 === 0 ) {
+        numerator = numerator / 2;
+        denominator = denominator / 2;
+    }
+    console.log(`Your sum is ${numerator} / ${denominator}`);
+}
+
---
+++ Lesson_34_Array/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let fruits = ["Apple", "Orange", "Plum"];
+
+// ітерується по елементам масиву
+for (let fruit of fruits) {
+    console.log( fruit );
+}
